462fef14b72b1ffb14b46041118adcd433a67715
Diego Vilarinho <contato@diegovilarinho.com.br>
2019-05-07 20:25:50 -0300

    initial commit

---
+++ src/main/java/com/diegovilarinho/smartspot/api/SmartspotApplication.java
@@ -0,0 +1,13 @@
+package com.diegovilarinho.smartspot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartspotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartspotApplication.class, args);
+    }
+
+}
